9008d05a5819599db2e85baeec911988d10e6f10
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-08-15 21:36:02 +0200

    db connecting test

--- database_basics/h2database/src/main/java/edu/yacoubi/h2database/Main.java
+++ database_basics/h2database/src/main/java/edu/yacoubi/h2database/Main.java
@@ -1,13 +1,29 @@
 package edu.yacoubi.h2database;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class Main {
+@RequiredArgsConstructor
+@Log
+public class Main implements CommandLineRunner {
+
+	private final DataSource dataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Datasource: " + dataSource.toString());
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+		restTemplate.execute("select 1");
+	}
 }
