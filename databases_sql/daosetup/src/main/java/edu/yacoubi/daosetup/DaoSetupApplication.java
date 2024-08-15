2cb64192610e1efb2dbd659b7ff0c6fd495d96d5
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-08-16 00:43:31 +0200

    setup

---
+++ databases_sql/daosetup/src/main/java/edu/yacoubi/daosetup/DaoSetupApplication.java
@@ -0,0 +1,13 @@
+package edu.yacoubi.daosetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaoSetupApplication.class, args);
+	}
+
+}
---
+++ databases_sql/daosetup/src/test/java/edu/yacoubi/daosetup/DaoSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.yacoubi.daosetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
