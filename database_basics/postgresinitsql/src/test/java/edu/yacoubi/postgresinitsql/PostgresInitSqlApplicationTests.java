c175d5b7dc60bdc4c07915c685e45ef0ae46126c
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-08-15 23:20:17 +0200

    sql data & schema init

---
+++ database_basics/postgresinitsql/src/test/java/edu/yacoubi/postgresinitsql/PostgresInitSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.yacoubi.postgresinitsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresInitSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
