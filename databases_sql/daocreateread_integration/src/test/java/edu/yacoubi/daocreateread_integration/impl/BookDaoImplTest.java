b69be6d9fc44e235e1c8ad332ca220c821fab586
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-08-16 15:01:05 +0200

    comments

--- databases_sql/daocreateread_integration/src/test/java/edu/yacoubi/daocreateread_integration/impl/BookDaoImplTest.java
+++ databases_sql/daocreateread_integration/src/test/java/edu/yacoubi/daocreateread_integration/impl/BookDaoImplTest.java
@@ -13,11 +13,24 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 
+/*
+ * This annotation is used to integrate Mockito with JUnit Jupiter,
+ * allowing for the use of Mockito's annotations like @Mock and @InjectMocks.
+ * */
 @ExtendWith(MockitoExtension.class)
 class BookDaoImplTest {
+    /*
+     * This annotation creates a mock object for the JdbcTemplate class.
+     * This allows you to test the behavior of the BookDaoImpl class
+     * without actually interacting with a real JdbcTemplate.
+     * */
     @Mock
     private JdbcTemplate jdbcTemplate;
 
+    /*
+     * This annotation injects the mock JdbcTemplate into the BookDaoImpl instance.
+     * This ensures that the BookDaoImpl class uses the mock JdbcTemplate during testing.
+     * */
     @InjectMocks
     private BookDaoImpl underTest;
 
@@ -33,6 +46,10 @@ class BookDaoImplTest {
         underTest.create(book);
 
         // Then
+        /*
+         * This method verifies that the update method of the JdbcTemplate is called with the expected arguments.
+         * This helps ensure that the BookDaoImpl class correctly generates the SQL statement for creating a book.
+         * */
         verify(jdbcTemplate).update(
                 eq("INSERT INTO books (isbn, title, author_id) VALUES (?, ?, ?)"),
                 eq("978-1-5498-6791-0"),
@@ -47,6 +64,11 @@ class BookDaoImplTest {
         underTest.find("978-1-5498-6791-0");
 
         // Then
+        /*
+         * This method verifies that the query method of the JdbcTemplate is called with the expected arguments.
+         * This helps ensure that the BookDaoImpl class correctly
+         * generates the SQL statement for finding a book by ISBN.
+         * */
         verify(jdbcTemplate).query(
                 eq("SELECT isbn, title, author_id FROM books WHERE isbn =? LIMIT 1"),
                 ArgumentMatchers.<BookDaoImpl.BookRowMapper>any(),
