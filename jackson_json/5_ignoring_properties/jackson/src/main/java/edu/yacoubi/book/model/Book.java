b8696980c8c6de36c6cd6bac89e46c9740a2127a
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-08-19 15:57:50 +0200

    jackson testing w. ignored properties demo setup

---
+++ jackson_json/5_ignoring_properties/jackson/src/main/java/edu/yacoubi/book/model/Book.java
@@ -0,0 +1,20 @@
+package edu.yacoubi.book.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+
+    @JsonProperty("year")
+    private String publicationYear;
+}
---
+++ jackson_json/5_ignoring_properties/jackson/src/test/java/edu/yacoubi/book/JacksonTest.java
@@ -0,0 +1,42 @@
+package edu.yacoubi.book;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.yacoubi.book.model.Book;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JacksonTest {
+    @Test
+    public void testThatObjectMapperCanCreateJsonObjectFromJavaObject() throws JsonProcessingException {
+        // Given
+        Book book = Book.builder()
+                .isbn("978-3-16-148410-0")
+                .title("Clean Code: A Handbook of Agile Software Craftsmanship")
+                .author("Robert C. Martin")
+                .publicationYear("2008")
+                .build();
+
+        // When
+        String json = new ObjectMapper().writeValueAsString(book);
+
+        // Then
+        assertThat(json).isEqualTo("{\"isbn\":\"978-3-16-148410-0\",\"title\":\"Clean Code: A Handbook of Agile Software Craftsmanship\",\"author\":\"Robert C. Martin\",\"year\":\"2008\"}");
+    }
+
+    @Test
+    public void testThatObjectMapperCanCreateJavaObjectFromJsonObject() throws JsonProcessingException {
+        // Given
+        String json = "{\"isbn\":\"978-3-16-148410-0\",\"title\":\"Clean Code: A Handbook of Agile Software Craftsmanship\",\"author\":\"Robert C. Martin\",\"year\":\"2008\"}";
+
+        // When
+        Book result = new ObjectMapper().readValue(json, Book.class);
+
+        // Then
+        assertThat(result.getIsbn()).isEqualTo("978-3-16-148410-0");
+        assertThat(result.getTitle()).isEqualTo("Clean Code: A Handbook of Agile Software Craftsmanship");
+        assertThat(result.getAuthor()).isEqualTo("Robert C. Martin");
+        assertThat(result.getPublicationYear()).isEqualTo("2008");
+    }
+}
