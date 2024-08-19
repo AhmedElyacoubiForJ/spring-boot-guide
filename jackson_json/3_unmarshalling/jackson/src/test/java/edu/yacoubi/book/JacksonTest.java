70df3d392b0ad6d0a4aac33656dbd07d52cf60f6
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-08-19 15:35:51 +0200

    testing creating java object from json object

--- jackson_json/3_unmarshalling/jackson/src/test/java/edu/yacoubi/book/JacksonTest.java
+++ jackson_json/3_unmarshalling/jackson/src/test/java/edu/yacoubi/book/JacksonTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class JacksonTest {
     @Test
-    public void testThatObjectMapperCanCreateJsonFromJavaObject() throws JsonProcessingException {
+    public void testThatObjectMapperCanCreateJsonObjectFromJavaObject() throws JsonProcessingException {
         // Given
         Book book = Book.builder()
                 .isbn("978-3-16-148410-0")
@@ -23,5 +23,20 @@ public class JacksonTest {
 
         // Then
         assertThat(json).isEqualTo("{\"isbn\":\"978-3-16-148410-0\",\"title\":\"Clean Code: A Handbook of Agile Software Craftsmanship\",\"author\":\"Robert C. Martin\",\"publicationYear\":\"2008\"}");
+    }
+
+    @Test
+    public void testThatObjectMapperCanCreateJavaObjectFromJsonObject() throws JsonProcessingException {
+        // Given
+        String json = "{\"isbn\":\"978-3-16-148410-0\",\"title\":\"Clean Code: A Handbook of Agile Software Craftsmanship\",\"author\":\"Robert C. Martin\",\"publicationYear\":\"2008\"}";
+
+        // When
+        Book result = new ObjectMapper().readValue(json, Book.class);
+
+        // Then
+        assertThat(result.getIsbn()).isEqualTo("978-3-16-148410-0");
+        assertThat(result.getTitle()).isEqualTo("Clean Code: A Handbook of Agile Software Craftsmanship");
+        assertThat(result.getAuthor()).isEqualTo("Robert C. Martin");
+        assertThat(result.getPublicationYear()).isEqualTo("2008");
     }
 }
