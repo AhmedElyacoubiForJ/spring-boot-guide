c81837b39e76a3ac31df22ce8d2b4a224c3b471f
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-08-20 13:05:57 +0200

    what we did before

--- rest_api/1_author_create/database/src/main/java/edu/yacoubi/database/repository/AuthorRepository.java
+++ rest_api/1_author_create/database/src/main/java/edu/yacoubi/database/repository/AuthorRepository.java
@@ -1,9 +1,15 @@
 package edu.yacoubi.database.repository;
 
 import edu.yacoubi.database.model.entity.Author;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthorRepository extends CrudRepository<Author, Long> {
+    Iterable<Author> findByNameContaining(String authorA);
+    Iterable<Author> ageLessThan(int age);
+
+    @Query("SELECT a FROM Author a WHERE age > ?1")
+    Iterable<Author> findAuthorsAgeGreaterThan(int age);
 }
