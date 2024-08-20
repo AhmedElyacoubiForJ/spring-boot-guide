a3dcecc0d459e630399663c6891b80377262c484
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-08-20 05:25:31 +0200

    refact

--- rest_api/3_book_create/database/src/main/java/edu/yacoubi/database/service/IBookService.java
+++ rest_api/3_book_create/database/src/main/java/edu/yacoubi/database/service/IBookService.java
@@ -3,5 +3,5 @@ package edu.yacoubi.database.service;
 import edu.yacoubi.database.model.entity.Book;
 
 public interface IBookService {
-    public Book createBook(String isbn, Book book);
+    Book createBook(String isbn, Book book);
 }
