ed2597873037d414fa824362d581e7b21ff816db
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-08-21 18:26:04 +0200

    nested objects book api project setup

---
+++ rest_api/14_nested_objects/database/src/main/java/edu/yacoubi/database/service/IBookService.java
@@ -0,0 +1,16 @@
+package edu.yacoubi.database.service;
+
+import edu.yacoubi.database.model.entity.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IBookService {
+    Book createUpdateBook(String isbn, Book book);
+    List<Book> getAll();
+    Optional<Book> getBook(String isbn);
+    boolean isExists(String isbn);
+    Book partialUpdate(String isbn, Book book);
+
+    void delete(String isbn);
+}
---
+++ rest_api/14_nested_objects/database/src/main/java/edu/yacoubi/database/service/impl/BookServiceImpl.java
@@ -0,0 +1,55 @@
+package edu.yacoubi.database.service.impl;
+
+import edu.yacoubi.database.model.entity.Book;
+import edu.yacoubi.database.repository.BookRepository;
+import edu.yacoubi.database.service.IBookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements IBookService {
+    private final BookRepository bookRepository;
+
+    @Override
+    public Book createUpdateBook(String isbn, Book book) {
+        book.setIsbn(isbn);
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> getAll() {
+        return StreamSupport.stream(bookRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<Book> getBook(String isbn) {
+        return bookRepository.findById(isbn);
+    }
+
+    @Override
+    public boolean isExists(String isbn) {
+        return bookRepository.existsById(isbn);
+    }
+
+    @Override
+    public Book partialUpdate(String isbn, Book book) {
+        book.setIsbn(isbn);
+
+        return bookRepository.findById(isbn).map(existingBook -> {
+            Optional.ofNullable(book.getTitle()).ifPresent(existingBook::setTitle);
+            return bookRepository.save(existingBook);
+        }).orElseThrow(() -> new RuntimeException("Book does not exist"));
+    }
+
+    @Override
+    public void delete(String isbn) {
+        bookRepository.deleteById(isbn);
+    }
+}
